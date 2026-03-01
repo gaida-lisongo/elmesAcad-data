d99068fbb7317fc0ab93a1f483ef5ee12ae14851
gaida-lisongo <inbtpkinshasa@gmail.com>
2026-03-01 10:57:37 +0100

    creating isolation uinite

--- src/app/(site)/unite/layout.tsx
+++ src/app/(site)/unite/layout.tsx
@@ -4,7 +4,7 @@ export default function UniteLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-gray-2 pt-16 dark:bg-boxdark-2 lg:pt-20">
+    <div className="min-h-screen bg-gray-2 pt-26 dark:bg-boxdark-2 lg:pt-20">
       {children}
     </div>
   );
